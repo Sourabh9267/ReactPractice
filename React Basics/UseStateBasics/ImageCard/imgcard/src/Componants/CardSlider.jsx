3430fda0e82d57520c5c6560a994e9d6b45d764e
Sourabh Gour <saurabhgour2222@gmail.com>
2024-09-28 01:33:27 +0530

    Slider Updated

--- React Basics/UseStateBasics/ImageCard/imgcard/src/Componants/CardSlider.jsx
+++ React Basics/UseStateBasics/ImageCard/imgcard/src/Componants/CardSlider.jsx
@@ -18,8 +18,8 @@ function CardSlider() {
             <IoIosArrowRoundForward  size={'4em'} />
 
             </button>
-            <img className={`overflow-hidden absolute z-0 transition-transform ${!val ? 'translate-x-[0%]' : '-translate-x[100%]'}`} src="https://images.unsplash.com/photo-1547405602-cdafbb1d8a44?q=80&w=1700&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
-            <img className={`overflow-hidden absolute z-0 transition-transform ${val ? '-translate-x-[100%]' : '-translate-x[0%]'}`} src="https://images.unsplash.com/photo-1544894079-e81a9eb1da8b?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+            <img className={`overflow-hidden absolute z-0 transition-transform delay-200 ${val ? 'translate-x-[0%]' : 'translate-x-[100%]'}`} src="https://images.unsplash.com/photo-1547405602-cdafbb1d8a44?q=80&w=1700&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+            <img className={`overflow-hidden absolute z-0 transition-transform delay-200 ${!val? '-translate-x-[0%]' : '-translate-x-[100%]'}`} src="https://images.unsplash.com/photo-1544894079-e81a9eb1da8b?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
 
 
 
