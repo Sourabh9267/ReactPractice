cf7ec7b1709c53c7b6a4c30ea07d705e017e9b31
Sourabh Gour <saurabhgour2222@gmail.com>
2024-10-17 01:16:51 +0530

    Ecommerce Store CRUD React Project is started

---
+++ ReactMiniProjects/03_E-CommerceStoreAPI/ecommstore/src/App.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className='h-screen w-full bg-red-100'>
+
+
+        <nav className='w-[15%] h-full bg-zinc-100 flex flex-col items-center'> 
+
+            
+            <a href="/create">Add New Product</a>
+            <h1>Category</h1>
+            <ul>
+                <li>Cat1</li>
+                <li>Cat2</li>
+                <li>Cat3</li>
+            </ul>
+
+
+        </nav>
+
+
+
+
+    </div>
+  )
+}
+
+export default App
